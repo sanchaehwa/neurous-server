615bdc5201d07c4d7538b8ff6481a48455310263
Hwa Young Yang <110398814+sanchaehwa@users.noreply.github.com>
2025-12-03 20:43:51 +0900

    chore: 프로젝트 환경 세팅

---
+++ src/main/java/com/example/gaje/GajeApplication.java
@@ -0,0 +1,13 @@
+package com.example.gaje;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GajeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GajeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gaje/GajeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gaje;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GajeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

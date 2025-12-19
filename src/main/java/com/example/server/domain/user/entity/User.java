040141745f9900a8a5d43b0c482382c606bcc6a5
Hwa Young Yang <110398814+sanchaehwa@users.noreply.github.com>
2025-12-19 23:40:26 +0900

    fix(user): User 엔티티 ERD 기준으로 수정
    - ERD 필드에 맞게 수정
    - 기존 @Setter 방식 제거
    - Enum 클래스 추가

--- src/main/java/com/example/server/domain/user/entity/User.java
+++ src/main/java/com/example/server/domain/user/entity/User.java
@@ -1,34 +1,84 @@
-package com.example.server.User.domain.entity;
+package com.example.server.domain.user.entity;
 
+import java.util.HashSet;
+import java.util.Set;
 
-import com.example.server.global.BaseTimeEntity;
-import jakarta.persistence.*;
-import lombok.*;
+import com.example.server.domain.user.entity.vo.Level;
+import com.example.server.domain.user.entity.vo.UserField;
+import com.example.server.domain.user.entity.vo.UserType;
+import com.example.server.global.domain.BaseTimeEntity;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "users")
 @Getter
-@Setter
 @Builder
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 public class User extends BaseTimeEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="user_id")
-    private int userId;
+	@Id
+	@Column(name = "user_id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    private String displayName;
-    private String email;
-    private String provider;     // google / kakao / naver
-    private String providerId;
-    private String imageFilename;
-    private String imageUrl;
-    private Boolean isActive;
-    private LocalDateTime nameUpdateAt; // displayName 변경 시 업데이트
+	@Column(nullable = false)
+	private String name;
+
+	@Column(unique = true, nullable = false)
+	private String userKey;
+
+	@Column //기본 이미지 설정
+	private String profileImgUrl;
+
+	@Column(unique = true, length = 50)
+	@Email
+	private String email;
+
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false, length = 25)
+	private UserType userType = UserType.USER;
+
+	@ElementCollection(fetch = FetchType.LAZY)
+	@CollectionTable(
+		name = "user_interests",
+		joinColumns = @JoinColumn(name = "user_id")
+	)
+	@Enumerated(EnumType.STRING)
+	@Column(name = "interest", nullable = false)
+	private Set<UserField> interests = new HashSet<>();
+
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private Level level = Level.BEGINNER; //기본값 : 초급
+
+	@Column(name = "is_activity")
+	private boolean isActive = true;
+
+	@Column(nullable = false, name = "notification_status")
+	private boolean notification_status = false; //알람 여부 미설정
+
+	//알림 여부 변경
+	public void toggleNotification() {
+		this.notification_status = !this.notification_status;
+	}
 
 }

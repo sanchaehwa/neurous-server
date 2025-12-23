05e0ab2753d46088a8953c4ba43a5054d9986e26
Hwa Young Yang <110398814+sanchaehwa@users.noreply.github.com>
2025-12-24 02:44:26 +0900

    #9 fix: User 엔티티 필드명 / 필드 수정 및 추가

--- src/main/java/com/example/server/domain/user/entity/User.java
+++ src/main/java/com/example/server/domain/user/entity/User.java
@@ -4,6 +4,8 @@ import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;
 
+import com.example.server.domain.auth.dto.OAuthUserInfo;
+import com.example.server.domain.auth.enums.OAuthProvider;
 import com.example.server.domain.user.entity.vo.Level;
 import com.example.server.domain.user.entity.vo.UserField;
 import com.example.server.domain.user.entity.vo.UserStatus;
@@ -45,8 +47,13 @@ public class User extends BaseTimeEntity {
 	@Column(nullable = false)
 	private String name;
 
-	@Column(unique = true, nullable = false)
-	private String userKey;
+	//소셜로그인
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private OAuthProvider provider;
+
+	@Column(nullable = false)
+	private String providerId;
 
 	@Column //기본 이미지 설정
 	private String profileImgUrl;
@@ -62,6 +69,7 @@ public class User extends BaseTimeEntity {
 	@Column(nullable = false, length = 25)
 	private UserType userType = UserType.USER;
 
+	//흥미
 	@ElementCollection(fetch = FetchType.LAZY)
 	@CollectionTable(
 		name = "user_interests",
@@ -75,23 +83,55 @@ public class User extends BaseTimeEntity {
 	@Column(nullable = false)
 	private Level level = Level.BEGINNER; //기본값 : 초급
 
-	@Column(name = "is_activity")
-	private boolean isActive = true;
+	@Column(nullable = false)
+	private boolean signUpComplete; //회원가입 이후 추가 정보까지 입력 여부
 
 	@Column(nullable = false, name = "notification_status")
-	private boolean notification_status = false; //알람 여부 미설정
+	private boolean notificationStatus = false; //알람 여부 미설정
 
 	private LocalDateTime lastLoginAt;
 
 	//알림 여부 변경
 	public void toggleNotification() {
-		this.notification_status = !this.notification_status;
+		this.notificationStatus = !this.notificationStatus;
 	}
 
-	public void updateOAuth2Info(String name, String email) {
-		this.name = name;
-		this.email = email;
-		this.lastLoginAt = LocalDateTime.now();
+	//todo: 프로필 이미지 추가정보 입력전에는 기본 이미지 처리
+	public static User create(
+		String name,
+		OAuthProvider provider,
+		String providerId,
+		String profileImgUrl,
+		String email
+	) {
+		return User.builder()
+			.name(name)
+			.provider(provider)
+			.providerId(providerId)
+			.profileImgUrl(profileImgUrl)
+			.email(email)
+			.status(UserStatus.NORMAL)
+			.userType(UserType.USER)
+			.interests(new HashSet<>())
+			.level(Level.BEGINNER)
+			.signUpComplete(false)
+			.notificationStatus(false)
+			.lastLoginAt(LocalDateTime.now())
+			.build();
 	}
 
+	public static User create(OAuthProvider provider, OAuthUserInfo oauthUserInfo) {
+		String name = oauthUserInfo.getName();
+		return User.create(
+			name,
+			provider,
+			oauthUserInfo.getProviderId(),
+			name,
+			oauthUserInfo.getEmail()
+		);
+	}
+
+	public boolean isSignUpComplete() {
+		return !signUpComplete;
+	}
 }

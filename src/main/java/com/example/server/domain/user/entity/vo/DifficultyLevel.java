d76391c89bb81d2dffab91db2b0d6dca8b38885e
Hwa Young Yang <110398814+sanchaehwa@users.noreply.github.com>
2025-12-24 19:32:08 +0900

    #9 feat: 난이도별 설명 테이블 생성

---
+++ src/main/java/com/example/server/domain/user/entity/vo/DifficultyLevel.java
@@ -0,0 +1,36 @@
+package com.example.server.domain.user.entity.vo;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+//레벨별 설명
+@Entity
+@Table(name = "level_definitions")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class DifficultyLevel {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private Level level;
+
+	@Column(nullable = false, columnDefinition = "TEXT")
+	private String description;
+
+	@Column(nullable = false, length = 500)
+	private String timeGuide;
+
+}

1465c13c0d60c58e0c443b4c287a988433cfdb9a
Hwa Young Yang <110398814+sanchaehwa@users.noreply.github.com>
2025-12-27 20:22:40 +0900

    #8 fix: JwtProperties 에 어노테이션 누락으로 Gradle 배포 에러 해결

--- src/main/java/com/example/server/global/security/jwt/JwtProperties.java
+++ src/main/java/com/example/server/global/security/jwt/JwtProperties.java
@@ -4,6 +4,9 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
+import lombok.Getter;
+
+@Getter
 @Component
 @ConfigurationProperties(prefix = "jwt")
 @Primary

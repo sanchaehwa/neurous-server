56a7cd794c8c20fe875d0ee1d4cc686af48a199c
Hwa Young Yang <110398814+sanchaehwa@users.noreply.github.com>
2025-12-27 20:18:49 +0900

    #8 fix: JwtProperties 에 @Primary 어노테이션 추가

--- src/main/java/com/example/server/global/security/jwt/JwtProperties.java
+++ src/main/java/com/example/server/global/security/jwt/JwtProperties.java
@@ -1,13 +1,12 @@
 package com.example.server.global.security.jwt;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
-import lombok.Getter;
-
-@Getter
 @Component
 @ConfigurationProperties(prefix = "jwt")
+@Primary
 public class JwtProperties {
 
 	private String secret;

d236decac9b12fc72648a31c308b4e0e25bbecdb
Hwa Young Yang <110398814+sanchaehwa@users.noreply.github.com>
2025-12-27 21:17:51 +0900

    #8 fix: @Setter 어노테이션 추가 (설정 값 주입 문제 해결

--- src/main/java/com/example/server/global/security/jwt/JwtProperties.java
+++ src/main/java/com/example/server/global/security/jwt/JwtProperties.java
@@ -3,8 +3,10 @@ package com.example.server.global.security.jwt;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter // 2. Setter 추가 (필수)
 @ConfigurationProperties(prefix = "jwt")
 public class JwtProperties {
 	private String secret;

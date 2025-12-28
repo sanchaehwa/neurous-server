440b8e95ea8096dfc5c9ce70543147b5ebbf170a
Hwa Young Yang <110398814+sanchaehwa@users.noreply.github.com>
2025-12-28 19:23:08 +0900

    #29  fix: @Component 어노테이션 추가로 빈등록

--- src/main/java/com/example/server/global/security/oauth/OAuthProperties.java
+++ src/main/java/com/example/server/global/security/oauth/OAuthProperties.java
@@ -1,34 +1,37 @@
 package com.example.server.global.security.oauth;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
-@Setter  // final을 제거하고 Setter를 추가합니다.
+@Setter
+@Component // 명시적으로 빈 등록
 @ConfigurationProperties(prefix = "oauth")
 public class OAuthProperties {
 
-	private Kakao kakao;
-	private Google google;
-	private Naver naver;
+	// 직접 초기화하여 NullPointerException 방지
+	private Kakao kakao = new Kakao();
+	private Google google = new Google();
+	private Naver naver = new Naver();
 
 	@Getter
 	@Setter
 	public static class Kakao {
-		private String userInfoUrl;
+		private String userInfoUrl = "https://kapi.kakao.com/v2/user/me";
 	}
 
 	@Getter
 	@Setter
 	public static class Google {
-		private String userInfoUrl;
+		private String userInfoUrl = "https://www.googleapis.com/oauth2/v2/userinfo";
 	}
 
 	@Getter
 	@Setter
 	public static class Naver {
-		private String userInfoUrl;
+		private String userInfoUrl = "https://openapi.naver.com/v1/nid/me";
 	}
 }

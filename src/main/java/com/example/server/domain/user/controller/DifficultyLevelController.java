433d2778c64754078e2e925af005dd5e1ca79f1e
Hwa Young Yang <110398814+sanchaehwa@users.noreply.github.com>
2025-12-31 18:15:00 +0900

    #36 fix: DifficultyLevel 컨트롤러 @RequestBody 에서 @PathVariable 로 변경

--- src/main/java/com/example/server/domain/user/controller/DifficultyLevelController.java
+++ src/main/java/com/example/server/domain/user/controller/DifficultyLevelController.java
@@ -1,19 +1,18 @@
 package com.example.server.domain.user.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.server.domain.user.controller.docs.DifficultyLevelControllerDocs;
-import com.example.server.domain.user.controller.dto.request.UpdateLevelRequest;
 import com.example.server.domain.user.controller.dto.response.LoadDifficultyLevel;
+import com.example.server.domain.user.entity.vo.Level;
 import com.example.server.domain.user.service.DifficultyLevelService;
 import com.example.server.global.exception.dto.SuccessResponse;
 import com.example.server.global.exception.message.SuccessMessage;
 import com.example.server.global.security.annotation.PublicApi;
 
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -26,10 +25,10 @@ public class DifficultyLevelController implements DifficultyLevelControllerDocs 
 	private final DifficultyLevelService difficultyLevelService;
 
 	@PublicApi(reason = "로그인을 하지않은 사용자도 레벨에 대한 정보를 조회 할 수 있습니다.")
-	@GetMapping
+	@GetMapping("/{level}")
 	public SuccessResponse<LoadDifficultyLevel> loadDifficultyLevel(
-		@Valid @RequestBody UpdateLevelRequest request) {
-		LoadDifficultyLevel data = difficultyLevelService.loadDifficultyLevel(request.level());
+		@PathVariable Level level) {
+		LoadDifficultyLevel data = difficultyLevelService.loadDifficultyLevel(level);
 		return SuccessResponse.of(SuccessMessage.LOAD_SUCCESS, data);
 	}
 }

7173a6260893c82ef1ce7d0a4d81d6875ca457dd
Hwa Young Yang <110398814+sanchaehwa@users.noreply.github.com>
2026-01-03 15:09:52 +0900

    #40 fix: JJWT 0.12.3 버전 문법 변경에 따른 컴파일 에러 수정

--- src/main/java/com/example/server/domain/auth/client/AppleApiClient.java
+++ src/main/java/com/example/server/domain/auth/client/AppleApiClient.java
@@ -67,7 +67,7 @@ public class AppleApiClient implements OAuthClient {
 	}
 
 	private Claims verifyToken(String identityToken) {
-		return Jwts.parserBuilder()
+		return Jwts.parser()
 			.build()
 			.parseClaimsJwt(identityToken)
 			.getBody();
